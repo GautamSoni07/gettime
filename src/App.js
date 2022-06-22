12aeb281e5082d453a71de0715a67c40324cbcba
GautamSoni07 <101924016+GautamSoni07@users.noreply.github.com>
2022-06-22 12:54:02 +0530

    Done

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+const App =() =>{
+
+  let time = new Date().toLocaleTimeString();
+
+  const [ctime, setCtime] = useState(time);
+
+const Updatetime = () =>{
+  time = new Date().toLocaleTimeString();
+  setCtime(time);
+};
+
+setInterval(Updatetime, 1000);
+  return(
+    <>
+    <h1> {ctime} </h1>
+    </>
+  );
+  };
+
+
+
+
+
+
+
+
+export default App;
